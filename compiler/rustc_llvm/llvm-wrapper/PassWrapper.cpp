df3a7a9151af588afd061d5b12b0c98075482939
Jack May <jack@solana.com>
2019-02-13 15:38:43 -0800

    [SOL] Enable LLVM backend

--- compiler/rustc_llvm/llvm-wrapper/PassWrapper.cpp
+++ compiler/rustc_llvm/llvm-wrapper/PassWrapper.cpp
@@ -169,6 +169,12 @@ extern "C" void LLVMRustTimeTraceProfilerFinish(const char *FileName) {
 #define SUBTARGET_LOONGARCH
 #endif
 
+#ifdef LLVM_COMPONENT_BPF
+#define SUBTARGET_BPF SUBTARGET(BPF)
+#else
+#define SUBTARGET_BPF
+#endif
+
 #define GEN_SUBTARGETS                                                         \
   SUBTARGET_X86                                                                \
   SUBTARGET_ARM                                                                \
@@ -184,7 +190,8 @@ extern "C" void LLVMRustTimeTraceProfilerFinish(const char *FileName) {
   SUBTARGET_HEXAGON                                                            \
   SUBTARGET_XTENSA                                                             \
   SUBTARGET_RISCV                                                              \
-  SUBTARGET_LOONGARCH
+  SUBTARGET_LOONGARCH                                                          \
+  SUBTARGET_BPF                                                                \
 
 #define SUBTARGET(x)                                                           \
   namespace llvm {                                                             \
