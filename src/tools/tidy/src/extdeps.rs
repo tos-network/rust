94a6c21f517125ba95c83be1a028a8726013380b
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-05-11 13:51:54 +0200

    [SOL] Fix github actions configuration

--- src/tools/tidy/src/extdeps.rs
+++ src/tools/tidy/src/extdeps.rs
@@ -8,6 +8,7 @@ const ALLOWED_SOURCES: &[&str] = &[
     r#""registry+https://github.com/rust-lang/crates.io-index""#,
     // This is `rust_team_data` used by `site` in src/tools/rustc-perf,
     r#""git+https://github.com/rust-lang/team#a5260e76d3aa894c64c56e6ddc8545b9a98043ec""#,
+    "\"git+https://github.com/solana-labs/compiler-builtins?tag=bpf-tools-v1.7#875087b410b77e0561b653882b431c84515eb044\""
 ];
 
 /// Checks for external package sources. `root` is the path to the directory that contains the
