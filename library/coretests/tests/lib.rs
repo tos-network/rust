9a25e76af3520e93281876c1e5b9d5d2a83be6b9
Alessandro Decina <alessandro.d@gmail.com>
2021-11-26 12:23:58 +1100

    [SOL] tests: disable failing core tests

--- library/coretests/tests/lib.rs
+++ library/coretests/tests/lib.rs
@@ -144,6 +144,7 @@ mod ascii;
 mod ascii_char;
 mod asserting;
 mod async_iter;
+#[cfg(not(target_arch = "bpf"))]
 mod atomic;
 mod bool;
 mod bstr;
@@ -155,19 +156,23 @@ mod const_ptr;
 mod convert;
 mod ffi;
 mod floats;
+#[cfg(not(target_arch = "bpf"))]
 mod fmt;
 mod future;
+#[cfg(not(target_arch = "bpf"))]
 mod hash;
 mod hint;
 mod intrinsics;
 mod io;
 mod iter;
+#[cfg(not(target_arch = "bpf"))]
 mod lazy;
 mod macros;
 mod manually_drop;
 mod mem;
 mod net;
 mod nonzero;
+#[cfg(not(target_arch = "bpf"))]
 mod num;
 mod ops;
 mod option;
@@ -175,9 +180,11 @@ mod panic;
 mod pattern;
 mod pin;
 mod pin_macro;
+#[cfg(not(target_arch = "bpf"))]
 mod ptr;
 mod result;
 mod simd;
+#[cfg(not(target_arch = "bpf"))]
 mod slice;
 mod str;
 mod str_lossy;
