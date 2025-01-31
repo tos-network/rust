2ecf17522fb2ee3c5aa4e292a44f8ef845eb671d
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2025-01-31 12:10:36 -0300

    [SOL] Test SBPFv1 and SBPFv2 in CI (#105)

--- src/ci/scripts/run-build-from-ci.sh
+++ src/ci/scripts/run-build-from-ci.sh
@@ -18,6 +18,14 @@ echo "::add-matcher::src/ci/github-actions/problem_matchers.json"
 rustup self uninstall -y || true
 if [ -z "${IMAGE+x}" ]; then
     src/ci/run.sh
+elif [ "$IMAGE" == "sbf-solana-solana-v1" ]; then
+    sed -i -e 's/.. Default::default()/cpu: "v1".into(), \n \t\t.. Default::default()/g' \
+     compiler/rustc_target/src/spec/base/sbf_base.rs
+    src/ci/docker/run.sh sbf-solana-solana
+elif [ "$IMAGE" == "sbf-solana-solana-v2" ]; then
+    sed -i -e 's/.. Default::default()/cpu: "v2".into(), \n \t\t.. Default::default()/g' \
+     compiler/rustc_target/src/spec/base/sbf_base.rs
+    src/ci/docker/run.sh sbf-solana-solana
 else
     src/ci/docker/run.sh "${IMAGE}"
 fi
